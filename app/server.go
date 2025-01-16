ca07e23fcba736468ae2cfc73ca84a7a6e0f1109
AkashKanteti <akashkanteti@gmail.com>
2025-01-17 00:10:04 +0530

    debud

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -36,18 +37,19 @@ func main() {
 	}
 
 	fmt.Printf("%v\n req", req)
-	// resp := make([]byte, 12)
+	resp := make([]byte, 12)
 
-	// binary.BigEndian.PutUint32(resp[0:4], uint32(42))
-	// copy(resp[4:8], req[8:12])
-	// binary.BigEndian.PutUint16(resp[8:10], uint16(0))
-	// // binary.BigEndian.PutUint16(resp[10:12], uint16(0))
+	binary.BigEndian.PutUint32(resp[0:4], uint32(42))
+	copy(resp[4:8], req[8:12])
+	binary.BigEndian.PutUint16(resp[8:10], uint16(0))
+	// binary.BigEndian.PutUint16(resp[10:12], uint16(0))
 
 	// _, err = conn.Write(resp)
 	// if err != nil {
 	// 	fmt.Println("Error writing data to connection: ", err.Error())
 	// 	os.Exit(1)
 	// }
+	fmt.Printf("%v\n resp", resp)
 
 	defer conn.Close()
 }
